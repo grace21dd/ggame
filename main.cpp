8d16ade44bea103b5b1f36b3d890ab36022e67dd
grace21dd <ngoisao00811@gmail.com>
2025-03-14 18:14:51 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 
 using namespace std;
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+class Game{
+public:
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    bool running;
+    //
+    Game()
+    {
+        running = true;
+        if
+    }
+    void render() {}
+    void run() {}
+    ~Game() {}
+
+
+};
 
 int main()
 {
